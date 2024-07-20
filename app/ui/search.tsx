916b3cbd99022f377fb9d7da344b3e45429d47b9
wayneGlfls <wayneni2@hotmail.com>
2024-07-20 14:36:01 +1000

    fixed default value null for search component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -49,7 +49,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
           handleSearch(e.target.value);
           //debounce(handleSearchNodebounce(e.target.value),300);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams ? searchParams.get('query')?.toString(): ''}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
