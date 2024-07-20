ace7c1264b9f2dd711b1721268988b9a40b24ee2
wayneGlfls <wayneni2@hotmail.com>
2024-07-20 14:26:46 +1000

    finxed  error for null scenario

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -18,6 +18,8 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
       const params = new URLSearchParams(searchParams);
       params.set('page', pageNumber.toString());
       return `${pathname}?${params.toString()}`;
+    }else{
+      return '';
     }
   };
 
