10677aec71c3a7e4acbeeaf48007df0dadfb2745
wayneGlfls <wayneni2@hotmail.com>
2024-07-27 19:00:33 +1000

    fix other errors

--- app/collaboration/page.tsx
+++ app/collaboration/page.tsx
@@ -1,20 +1,29 @@
 'use server';
 
 import { AblyClient } from "../ui/collaboration/AblyPubSub";
-import { fetchCustomers,fetchChatMessage } from "../lib/data";
+import { fetchCustomers, fetchChatMessage } from "../lib/data";
 import { ChatMessageTable } from "../lib/definitions";
-import { getUIUser,auth } from "auth";
+import { getUIUser, auth } from "auth";
 
-
-export default async function Page() {
-
-    const chatdata : ChatMessageTable[] = await fetchChatMessage();
-    //const user = getUser(auth());
-    const session = await auth();
-    const user = await getUIUser(session?.user?.email);
-  return (<>
-    <AblyClient messagehistory={chatdata} user={user}/>
-    </>
-  );
+// Define the User type
+interface User {
+    id: string;
+    email: string;
+    name: string;
+    image_url: string;
 }
 
+export default async function Page() {
+    // Fetch chat messages
+    const chatdata: ChatMessageTable[] = await fetchChatMessage();
+
+    // Authenticate the user and get user details
+    const session = await auth();
+    const user: User = await getUIUser(session.user.email);
+
+    return (
+        <>
+            <AblyClient messagehistory={chatdata} user={user} />
+        </>
+    );
+}
